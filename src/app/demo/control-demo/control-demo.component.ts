38e67e17bc53828e5f033818dd7dfb5ccf25a0a4
pkthong12 <129492809+pkthong12@users.noreply.github.com>
2025-03-03 21:47:17 +0700

    update demo

--- src/app/demo/control-demo/control-demo.component.ts
+++ src/app/demo/control-demo/control-demo.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { CoreFormComponent } from '../../libraries/core-form/core-form.component';
-import { EnumFormBaseControlType, ICoreFormSection, IFnNameValidator } from '../../enum/enum-interfaces';
+import { EnumFormBaseControlType, ICoreFormSection, IFnNameValidator, IFormBaseControl } from '../../enum/enum-interfaces';
 import { Validators } from '@angular/forms';
 
 @Component({
@@ -13,21 +13,200 @@ import { Validators } from '@angular/forms';
   styleUrl: './control-demo.component.scss'
 })
 export class ControlDemoComponent {
-  
+
   formName = 'DEMO';
 
+  sectionsTxt: ICoreFormSection[] = [
+    {
+      caption: 'For TextBox',
+      rows: [
+        [
+          {
+            controlType: EnumFormBaseControlType.TEXTBOX,
+            field: 'controlType',
+            label: 'controlType',
+            value: 'TEXTBOX',
+            type: 'text',
+            flexSize: 12,
+            readonly: false,
+            hidden: true,
+          },
+          {
+            controlType: EnumFormBaseControlType.TEXTBOX,
+            field: 'textbox',
+            label: 'field',
+            value: 'TEXTBOX',
+            type: 'text',
+            flexSize: 12,
+            readonly: false,
+            hidden: true,
+          },
+          {
+            controlType: EnumFormBaseControlType.TEXTBOX,
+            field: 'label',
+            label: 'Label',
+            value: '',
+            type: 'text',
+            flexSize: 12,
+            readonly: false,
+            hidden: false,
+          },
+          {
+            controlType: EnumFormBaseControlType.TEXTBOX,
+            field: 'type',
+            label: 'Type',
+            value: null,
+            type: 'text',
+            flexSize: 12,
+            readonly: false,
+            hidden: false,
+          },
+          {
+            controlType: EnumFormBaseControlType.TEXTBOX,
+            field: 'flexSize',
+            label: 'Flex Size',
+            value: null,
+            type: 'number',
+            flexSize: 12,
+            readonly: false,
+            hidden: false,
+          },
+          {
+            controlType: EnumFormBaseControlType.CHECKBOX,
+            field: 'required',
+            label: 'Required',
+            value: false,
+            type: 'boolean',
+            flexSize: 4,
+            readonly: false,
+            hidden: false,
+          },
+          {
+            controlType: EnumFormBaseControlType.CHECKBOX,
+            field: 'disabled',
+            label: 'Disabled',
+            value: false,
+            type: 'boolean',
+            flexSize: 4,
+            readonly: false,
+            hidden: false,
+          },
+          {
+            controlType: EnumFormBaseControlType.CHECKBOX,
+            field: 'readonly',
+            label: 'Readonly',
+            value: false,
+            type: 'boolean',
+            flexSize: 4,
+            readonly: false,
+            hidden: false,
+          },
+          {
+            controlType: EnumFormBaseControlType.CHECKBOX,
+            field: 'hidden',
+            label: 'Hidden',
+            value: false,
+            type: 'boolean',
+            flexSize: 4,
+            readonly: false,
+            hidden: false,
+          }
+        ]
+      ]
+    }
+  ];
+  sectionsCkb: ICoreFormSection[] = [
+    {
+      caption: 'For Checkbox',
+      rows: [
+        [
+          {
+            controlType: EnumFormBaseControlType.TEXTBOX,
+            field: 'controlType',
+            label: 'Label',
+            value: 'CHECKBOX',
+            type: 'text',
+            flexSize: 12,
+            readonly: false,
+            hidden: true,
+          },
+          {
+            controlType: EnumFormBaseControlType.TEXTBOX,
+            field: 'checkbox',
+            label: 'field',
+            value: 'TEXTBOX',
+            type: 'text',
+            flexSize: 12,
+            readonly: false,
+            hidden: true,
+          },
+          {
+            controlType: EnumFormBaseControlType.TEXTBOX,
+            field: 'label',
+            label: 'Label',
+            value: '',
+            type: 'text',
+            flexSize: 12,
+            readonly: false,
+            hidden: false,
+          },
+          {
+            controlType: EnumFormBaseControlType.TEXTBOX,
+            field: 'flexSize',
+            label: 'Flex Size',
+            value: null,
+            type: 'number',
+            flexSize: 12,
+            readonly: false,
+            hidden: false,
+          },
+          {
+            controlType: EnumFormBaseControlType.CHECKBOX,
+            field: 'disabled',
+            label: 'Disabled',
+            value: false,
+            type: 'boolean',
+            flexSize: 4,
+            readonly: false,
+            hidden: false,
+          },
+          {
+            controlType: EnumFormBaseControlType.CHECKBOX,
+            field: 'readonly',
+            label: 'Readonly',
+            value: false,
+            type: 'boolean',
+            flexSize: 4,
+            readonly: false,
+            hidden: false,
+          },
+          {
+            controlType: EnumFormBaseControlType.CHECKBOX,
+            field: 'hidden',
+            label: 'Hidden',
+            value: false,
+            type: 'boolean',
+            flexSize: 4,
+            readonly: false,
+            hidden: false,
+          }
+        ]
+      ]
+    }
+  ];
   sections: ICoreFormSection[] = [
     {
+      caption: 'For Preview',
       rows: [
         [
           {
             controlType: EnumFormBaseControlType.TEXTBOX,
             field: 'textbox',
             label: 'TEXTBOX',
-            value: 'value Textbox',
+            value: '',
             type: 'text',
-            flexSize: 3,
-            readonly: true,
+            flexSize: 4,
+            readonly: false,
             hidden: false,
           },
           {
@@ -36,50 +215,37 @@ export class ControlDemoComponent {
             label: 'CHECKBOX',
             value: false,
             type: 'boolean',
-            flexSize: 3,
+            flexSize: 4,
             readonly: false,
             hidden: false,
-            disabled: true
-          },
-          {
-            controlType: EnumFormBaseControlType.TEXTBOX,
-            field: 'textbox1',
-            label: 'TEXTBOX',
-            value: '',
-            type: 'text',
-            flexSize: 3,
-            readonly: false,
-            hidden: true,
-          }, {
-            controlType: EnumFormBaseControlType.TEXTBOX,
-            field: 'textbox2',
-            label: 'TEXTBOX',
-            value: '',
-            type: 'number',
-            flexSize: 3,
-            readonly: false,
-            hidden: false,
-            validators: [
-              {
-                name: IFnNameValidator.required,
-                validator: Validators.required,
-                errorMessage: 'This field is required'
-              },
-              {
-                name: IFnNameValidator.min,
-                validator: Validators.min(18),
-                errorMessage: 'You must be at least 18 years old'
-              }
-            ]
-          },
+          }
         ]
       ]
     }
   ];
 
-  onSubmit($event: any) {
-    console.log($event)
+  onSubmitTxt($event: any) {
+    let control: IFormBaseControl = this.convertToControl($event);
+    this.sections[0].rows[0][0] = control;
+  }
+
+
+  onSubmitCkb($event: any) {
+    let control: IFormBaseControl = this.convertToControl($event);
+    this.sections[0].rows[0][1] = control;
   }
   onCancel($event: any) {
   }
+  convertToControl(control: any): IFormBaseControl {
+    control.validators = [];
+    if (control.required) {
+      control.validators.push({
+        name: IFnNameValidator.required,
+        validator: Validators.required,
+        errorMessage: 'This field is required'
+      })
+    }
+    return control as IFormBaseControl;
+  }
+
 }
--- src/app/enum/enum-interfaces.ts
+++ src/app/enum/enum-interfaces.ts
@@ -35,6 +35,7 @@ export interface IValidator {
 }
 
 export interface ICoreFormSection {
+  caption?: string;
   rows: IFormBaseControl[][];
 }
 
