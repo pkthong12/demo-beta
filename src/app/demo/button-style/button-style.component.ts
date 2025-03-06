c7d3dae155390f1f122e7bf1a1f63e66bc4396bc
pkthong12 <129492809+pkthong12@users.noreply.github.com>
2025-03-06 17:06:48 +0700

    update

--- src/app/demo/button-style/button-style.component.ts
+++ src/app/demo/button-style/button-style.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-button-style',
   standalone: true,
   imports: [],
   templateUrl: './button-style.component.html',
-  styleUrl: './button-style.component.scss'
+  styleUrl: './button-style.component.scss',
+  encapsulation: ViewEncapsulation.ShadowDom
 })
 export class ButtonStyleComponent {
 
